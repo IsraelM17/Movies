ca480ff77df4dd92d805cef321da7b6a6a41c81a
IsraelM17 <se.israel.mc@gmail.com>
2019-01-24 13:11:42 -0600

    Primera fase para reproduccion de video en el widget

---
+++ lib/video.dart
@@ -0,0 +1,134 @@
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:custom_chewie/custom_chewie.dart';
+
+class ChewieDemo extends StatefulWidget {
+  final String title;
+
+  ChewieDemo({this.title = 'Chewie Demo'});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ChewieDemoState();
+  }
+}
+
+class _ChewieDemoState extends State<ChewieDemo> {
+  TargetPlatform _platform;
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      'https://media.w3.org/2010/05/sintel/trailer.mp4',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: widget.title,
+      theme: ThemeData.light().copyWith(
+        platform: _platform ?? Theme.of(context).platform,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Chewie(
+                  _controller,
+                  aspectRatio: 3 / 2,
+                  autoPlay: true,
+                  looping: true,
+
+                  // Try playing around with some of these other options:
+
+                  // showControls: false,
+                  // materialProgressColors: ChewieProgressColors(
+                  //   playedColor: Colors.red,
+                  //   handleColor: Colors.blue,
+                  //   backgroundColor: Colors.grey,
+                  //   bufferedColor: Colors.lightGreen,
+                  // ),
+                  // placeholder: Container(
+                  //   color: Colors.grey,
+                  // ),
+                  // autoInitialize: true,
+                ),
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _controller = VideoPlayerController.network(
+                          'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+                        );
+                      });
+                    },
+                    child: Padding(
+                      child: Text("Video 1"),
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _controller = VideoPlayerController.network(
+                          'https://www.sample-videos.com/video123/mp4/480/big_buck_bunny_480p_20mb.mp4',
+                        );
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("Video 2"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _platform = TargetPlatform.android;
+                      });
+                    },
+                    child: Padding(
+                      child: Text("Android controls"),
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _platform = TargetPlatform.iOS;
+                      });
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      child: Text("iOS controls"),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
