3641344e448e6b4008cd69c89fe0be6243df3527
IsraelM17 <se.israel.mc@gmail.com>
2019-02-12 12:04:46 -0600

    Tema personalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:videos/home.dart';
 import 'package:videos/movie_detail.dart';
+import 'package:videos/home.dart';
+import 'package:videos/login.dart';
 
 var routes = <String, WidgetBuilder>{
   "/movieDetail" : (BuildContext context) => MovieDetail(),
+  "/home" : (BuildContext context) => Home()
 };
 
 void main() {
   runApp(new MaterialApp(
-   home: Home(),
+   home: Login(),
    theme: ThemeData(
+     primaryColor: Colors.black,
+     brightness: Brightness.dark,
+     textTheme: TextTheme(
+       body1: TextStyle(color: Colors.white)
+     ),
      fontFamily: 'Nunito'
    ),
    routes: routes,
